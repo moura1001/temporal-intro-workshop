6e0a6ba108f2496963cb8d498ae0ba7cbafa05b4
moura1001 <gmouraneto01@gmail.com>
2022-07-21 18:31:04 -0300

    commit com finalização do exercício 1 do Workshop: implementação de um workflow

--- workflows/workflow.go
+++ workflows/workflow.go
@@ -1,9 +1,35 @@
 package workflows
 
-import "go.temporal.io/sdk/workflow"
+import (
+	"errors"
 
-func Workflow(ctx workflow.Context) error {
+	"go.temporal.io/sdk/workflow"
+)
+
+type Input struct {
+	Number int
+}
+
+type Output struct {
+	Result int
+}
+
+func Workflow(ctx workflow.Context, input Input) (Output, error) {
 	workflow.GetLogger(ctx).Info("starting workflow")
 
-	return nil
+	if input.Number < 1 {
+		return Output{}, errors.New("invalid number")
+	}
+
+	result := 1
+
+	for i := 1; i <= input.Number; i++ {
+		result *= i
+	}
+
+	output := Output{
+		Result: result,
+	}
+
+	return output, nil
 }
